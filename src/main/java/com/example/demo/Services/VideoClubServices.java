357264d59c4be45c58e656059ad757a86f369fa3
Haris Patsouras <haris.p.dev@gmail.com>
2026-06-03 22:53:09 +0300

    first commit

---
+++ src/main/java/com/example/demo/Services/VideoClubServices.java
@@ -0,0 +1,12 @@
+package com.example.demo.Services;
+
+import com.example.demo.DTO.MovieDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideoClubUtils {
+
+    private List<MovieDto> movies = new ArrayList<>();
+
+}
